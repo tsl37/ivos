ab5c82ab978f494471d890b0c355b0e1d6e3178f
tsl37 <129293457+tsl37@users.noreply.github.com>
2026-05-12 17:46:10 +0200

    Delete scheduler directory

--- scheduler/gthr_struct.h
+++
@@ -1,5 +0,0 @@
-#include <sys/time.h> 
-// gthread control structures
-
-struct gt gt_table[MaxGThreads];                                                // statically allocated table for thread control
-struct gt *gt_current;                                                          // pointer to current thread
